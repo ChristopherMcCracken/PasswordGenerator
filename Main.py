2795ce5495dd39910fa6b9d4f316971a13523e00
Jordan Richardson <jordanr2342@gmail.com>
2019-11-17 16:30:37 -0700

    Created config file to store different password lengths

--- Main.py
+++ Main.py
@@ -2,48 +2,17 @@ import facial_recog
 from GrabUrl import grabUrl
 from GenerateKey import generateKey
 from url_regex import urlRegex
+from config import getSiteLength
 import pyperclip
-from argparse import ArgumentParser
-import sys
 import cv2          # import for the error catch
 
 
 # -------------------------------------------------------------------------------------------------------------------- #
 def run():
-    # parser is used for the password_size argument
-    parser = ArgumentParser()
-    parser.add_argument('--password_size', help='Set the length of the password. Defaults to 32.')
-
-    arguments = parser.parse_args()
-
-    MIN_PASSWORD_LENGTH = 7
-    MAX_PASSWORD_LENGTH = 256
-    DEFAULT_PASSWORD_LENGTH = 32
-
-    # check if the password_size argument exists
-    if arguments.password_size:
-        # try to convert the argument to an int
-        try:
-            passwordSize = int(arguments.password_size)
-        # except ValueError, which means the user entered more than just numbers
-        except ValueError:
-            print('Invalid password size')
-            sys.exit(1)
-
-        # check if the password length is too large or small
-        if passwordSize < MIN_PASSWORD_LENGTH:
-            print('Password length is too small')
-            sys.exit(2)
-        elif passwordSize > MAX_PASSWORD_LENGTH:
-            print('Password length is too large')
-            sys.exit(3)
-    # if not set the password to the default password length
-    else:
-        passwordSize = DEFAULT_PASSWORD_LENGTH
-
     print("Generating Password...\n")
     url = grabUrl()
     parsedURL = urlRegex(url)
+    passwordSize = getSiteLength(parsedURL)
 
     try:
         face = facial_recog.facialRecognition()
---
+++ config.py
@@ -0,0 +1,63 @@
+"""
+config.py - used to parse a config file that will hold the password lengths for different websites.
+The file can be modified incase users want a longer or shorter password
+"""
+
+import configparser
+from os import path
+
+CONFIG_FILE_NAME = 'config.ini'
+MIN_PASSWORD_LENGTH = 7
+MAX_PASSWORD_LENGTH = 256
+DEFAULT_PASSWORD_LENGTH = 32
+
+def getSiteLength(site):
+    """
+    attempts to get the site length from the config file. If it is unable to, it will update or
+    create the config file and store the length for the site.
+    """
+    if not path.isfile(CONFIG_FILE_NAME):
+        createConfigFile()
+
+    config = configparser.ConfigParser()
+    config.read(CONFIG_FILE_NAME)
+
+    if site in config['Sites']:
+        try:
+            siteLength = config.getint('Sites', site)
+        except ValueError:
+            print(f'Invalid length for {site}.\nSite length must be an integer value.')
+            exit()
+
+        if siteLength > MAX_PASSWORD_LENGTH:
+            return MAX_PASSWORD_LENGTH
+        elif siteLength < MIN_PASSWORD_LENGTH:
+            return MIN_PASSWORD_LENGTH
+        else:
+            return siteLength
+    else:
+        updateConfigFile(site)
+        return DEFAULT_PASSWORD_LENGTH
+
+def updateConfigFile(site):
+    """
+    updates the config file with the site length
+    """
+    configUpdate = configparser.RawConfigParser()
+    configUpdate.read(CONFIG_FILE_NAME)
+
+    configUpdate.set('Sites', site, DEFAULT_PASSWORD_LENGTH)
+
+    with open(CONFIG_FILE_NAME, 'w') as configfile:
+        configUpdate.write(configfile)
+
+def createConfigFile():
+    """
+    creates the config file if it doesn't exist
+    """
+    configCreation = configparser.RawConfigParser()
+
+    configCreation.add_section('Sites')
+
+    with open(CONFIG_FILE_NAME, 'w') as configfile:
+        configCreation.write(configfile)
