108daddd5587cb945f005aaecdfb683b7973c977
QBar <qbarrette@gmail.com>
2019-11-21 18:15:21 -0700

    Added Fernet Encryption, selenium webdriver to grab URL from Chrome

--- GenerateKey.py
+++ GenerateKey.py
@@ -2,6 +2,7 @@ import base64
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.fernet import Fernet
 
 
 def generateKey(url, passwordSize, face, pin):
@@ -17,7 +18,10 @@ def generateKey(url, passwordSize, face, pin):
         backend=default_backend()
     )
 
-    key = base64.urlsafe_b64encode(kdf.derive(password))  # Can only use kdf once
+    kdf_key = base64.urlsafe_b64encode(kdf.derive(password))  # Can only use kdf once
+
+    # Fernet is used to encode again as it provides extra security for the password
+    key = Fernet(kdf_key)
     print(f'Your key is: {key}\n')
     return key
 
--- GrabUrl.py
+++ GrabUrl.py
@@ -1,6 +1,7 @@
 import time
 import keyboard as keyboard
 import pyperclip
+from selenium import webdriver
 
 
 def grabUrl():
@@ -14,7 +15,17 @@ def grabUrl():
     # keyboard.press_and_release('alt+tab')
     # time.sleep(.1)
 
+    #selenium to grab current URL from Chrome browser
+    # will need chromedriver in the PATH for it to talk to chrome
+    # Can specify path to webdriver instead of adding to PATH
+    driver = webdriver.Chrome()     #leave blank to have it search within PATH to find chromedriver
+    url = driver.getCurrentUrl()
+
+    #firefox implementation
+    #firefox_driver = webdriver.Firefox()
+    #firefox_url = driver.getCurrentUrl()
+
     # Store URL
-    url = pyperclip.paste()
+    #url = pyperclip.paste()
     print("Your current url is: " + url + "\n")
     return url
